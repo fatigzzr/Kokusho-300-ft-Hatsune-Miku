f2b3538bd2574af8644b21c74c16b455b532c9a5
fatigzzr <fatima.gonzalezr@udem.edu>
2025-04-23 23:50:23 -0600

    first commit

---
+++ app/src/androidTest/java/fatima/m596749/einer/m595839/kokusho_300_ft_hatsune_miku/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package fatima.m596749.einer.m595839.kokusho_300_ft_hatsune_miku
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("fatima.m596749.einer.m595839.myapplication", appContext.packageName)
+    }
+}
---
+++ app/src/test/java/fatima/m596749/einer/m595839/kokusho_300_ft_hatsune_miku/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package fatima.m596749.einer.m595839.kokusho_300_ft_hatsune_miku
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
