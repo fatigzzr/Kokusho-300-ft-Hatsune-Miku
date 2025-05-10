317b7442d422d9052224c66f32e95215b0f87744
fatigzzr <fatima.gonzalezr@udem.edu>
2025-05-09 22:27:25 -0600

    conflicts resolved

--- app/src/main/java/fatima/m596749/einer/m595839/kokusho_300_ft_hatsune_miku/database/KanjiDao.kt
+++ app/src/main/java/fatima/m596749/einer/m595839/kokusho_300_ft_hatsune_miku/database/KanjiDao.kt
@@ -1,5 +1,6 @@
 package fatima.m596749.einer.m595839.kokusho_300_ft_hatsune_miku.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
